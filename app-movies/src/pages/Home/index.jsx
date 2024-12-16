910b5d4da7cc32ba926d29119fb5a80ae7b9c370
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2024-12-16 20:39:32 -0300

    arrumando container

--- app-movies/src/pages/Home/index.jsx
+++ app-movies/src/pages/Home/index.jsx
@@ -1,24 +1,28 @@
 import {Link} from 'react-router-dom'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import Container from '../../components/Container'
+import styles from './Home.module.css'
 
 function Home(){
     return(
         <>
       <Header/>
-      <section className='container'>
-         <div className='apresentacao'>
-            <p>
-              olá, sou <br/>
-              <span>Luan Rodrigues</span> <br />
-               Desenvolvedor Web
-            </p>
-            <Link to="/sobre"><button className='btn btn-blue'>Saiba mais</button></Link>
-         </div>
-         <figure>
-          <img className='img-home' src="/developer-blue.svg" alt="imagem-home" />
-         </figure>
-      </section>
+      <Container>
+            <section className={styles.home}>
+               <div className={styles.apresentacao}>
+                  <p>
+                  olá, sou <br/>
+                  <span>Luan Rodrigues</span> <br />
+                     Desenvolvedor Web
+                  </p>
+                  <Link to="/sobre"><button className={`${styles.btn} ${styles.btn_blue} ${styles.btn_blue_hover}`}>Saiba mais</button></Link>
+               </div>
+               <figure>
+               <img className={styles.img_home} src="/developer-blue.svg" alt="imagem-home" />
+               </figure>
+            </section>
+      </Container>
       <Footer/>
     </>
     )
