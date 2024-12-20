81c83443a3f2c04c0387516cf8e7f6ec0d70736e
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2024-12-19 21:31:51 -0300

    Rotas aninhadas

--- app-movies/src/pages/Home/index.jsx
+++ app-movies/src/pages/Home/index.jsx
@@ -1,30 +1,25 @@
 import {Link} from 'react-router-dom'
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-import Container from '../../components/Container'
 import styles from './Home.module.css'
 
 function Home(){
     return(
-        <>
-      <Header/>
-      <Container>
-            <section className={styles.home}>
-               <div className={styles.apresentacao}>
-                  <p>
-                  olá, sou <br/>
-                  <span>Luan Rodrigues</span> <br />
-                     Desenvolvedor Web
-                  </p>
-                  <Link to="/sobre"><button className={`${styles.btn} ${styles.btn_blue} ${styles.btn_blue_hover}`}>Saiba mais</button></Link>
-               </div>
-               <figure>
-               <img className={styles.img_home} src="/developer-blue.svg" alt="imagem-home" />
-               </figure>
-            </section>
-      </Container>
-      <Footer/>
-    </>
+        
+      
+         <section className={styles.home}>
+            <div className={styles.apresentacao}>
+               <p>
+               olá, sou <br/>
+               <span>Luan Rodrigues</span> <br />
+                  Desenvolvedor Web
+               </p>
+               <Link to="/sobre"><button className={`${styles.btn} ${styles.btn_blue} ${styles.btn_blue_hover}`}>Saiba mais</button></Link>
+            </div>
+            <figure>
+            <img className={styles.img_home} src="/developer-blue.svg" alt="imagem-home" />
+            </figure>
+         </section>
+      
+    
     )
 }
 
--- app-movies/src/pages/Page404/index.jsx
+++ app-movies/src/pages/Page404/index.jsx
@@ -1,21 +1,17 @@
-import Container from "../../components/Container"
-import Footer from "../../components/Footer"
-import Header from "../../components/Header"
+
 import style from "./Page404.module.css"
 
 function Page404(){
      return(
         <>
-        <Header/>
-        <Container>
-             <h2 className={style.titulo2}>Algo de errado não está certo!</h2>
-             <div className={style.textos}>
-                 <span className={style.texto_grande}>404</span> <br/>
-                 <strong className={style.texto_cor}>Página não pode ser encontrada!</strong>
-            
-              </div>
-        </Container>
-        <Footer/>
+             
+         <h2 className={style.titulo2}>Algo de errado não está certo!</h2>
+         <div className={style.textos}>
+            <span className={style.texto_grande}>404</span> <br/>
+            <strong className={style.texto_cor}>Página não pode ser encontrada!</strong>
+      
+         </div>
+   
         </>
      )
 
