fa64b0f28eda46d6e3d157861b5f3e15b5066b86
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2024-12-23 21:16:23 -0300

    avatar

--- app-movies/src/pages/Sobre/index.jsx
+++ app-movies/src/pages/Sobre/index.jsx
@@ -1,14 +1,20 @@
+import styles from './Sobre.module.css'
+import eu from './images/eu.png'
 
 function Sobre(){
     return (
-       <>
-       
-            
-           <h2>Sobre</h2>
-
-       
-       
-       </>
+       <section className={styles.Sobre}>
+           <div className={styles.bio}> 
+              <img src={eu} alt="avatar" className={styles.avatar} />
+              <div>
+                 <h2>Sobre</h2>
+              </div>
+           </div> 
+           <div className={styles.techs}>
+              <h3>Techs</h3>
+              images
+           </div>
+       </section>
     )
 }
 
