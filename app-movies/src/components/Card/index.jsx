2d837b2939f48bb1cc0a7cf99291cba81a68ab58
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2024-12-27 20:05:23 -0300

    card

--- app-movies/src/components/Card/index.jsx
+++ app-movies/src/components/Card/index.jsx
@@ -2,7 +2,19 @@ import style from './Card.module.css'
 
 function Card() {
     return(
+       <section className={style.card}>
+          <h3>Título do projeto</h3>
+          <p>Texto descritivo do projeto</p>
+          <div className={style.card_footer}>
+               <div className={style.card_icones}>
+                       HTML
+               </div>
+               <button className={style.card_botao}>
+                   seta
+               </button>
+          </div>
 
+       </section>  
     )
 }
 
--- app-movies/src/pages/Projetos/index.jsx
+++ app-movies/src/pages/Projetos/index.jsx
@@ -1,10 +1,13 @@
+import Card from "../../components/Card"
 
 function Projetos(){
     return (
         <>
        
            <h2>Projetos</h2>
-        
+            <Card/>
+            <Card/>
+            <Card/>
         
         
         </>
