189c1ff399835019737ece705e0e3f411ffed668
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2024-12-30 19:54:53 -0300

    card

--- app-movies/src/components/Card/index.jsx
+++ app-movies/src/components/Card/index.jsx
@@ -1,16 +1,25 @@
 import style from './Card.module.css'
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa";
+import { LuSquareArrowRight } from "react-icons/lu";
 
 function Card() {
     return(
        <section className={style.card}>
           <h3>Título do projeto</h3>
-          <p>Texto descritivo do projeto</p>
+          <p>Texto descritivo do projeto.</p>
           <div className={style.card_footer}>
                <div className={style.card_icones}>
-                       HTML
+                   <FaHtml5 />
+                   <FaCss3Alt />
+                   <IoLogoJavascript />
+                   <FaReact />
+                  
                </div>
                <button className={style.card_botao}>
-                   seta
+                  <LuSquareArrowRight />
                </button>
           </div>
 
