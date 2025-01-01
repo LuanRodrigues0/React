ac0afd49b323102fa5b201df7973970c20991542
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2025-01-01 00:42:21 -0300

    repositores

--- app-movies/src/components/Card/index.jsx
+++ app-movies/src/components/Card/index.jsx
@@ -4,12 +4,14 @@ import { FaCss3Alt } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import { FaReact } from "react-icons/fa";
 import { LuSquareArrowRight } from "react-icons/lu";
+import { Link } from 'react-router-dom';
 
-function Card() {
+// eslint-disable-next-line react/prop-types
+function Card({name, description, html_url}) {
     return(
        <section className={style.card}>
-          <h3>Título do projeto</h3>
-          <p>Texto descritivo do projeto.</p>
+          <h3>{name}</h3>
+          <p>{description}</p>
           <div className={style.card_footer}>
                <div className={style.card_icones}>
                    <FaHtml5 />
@@ -18,9 +20,9 @@ function Card() {
                    <FaReact />
                   
                </div>
-               <button className={style.card_botao}>
+               <Link to={html_url} className={style.card_botao}>
                   <LuSquareArrowRight />
-               </button>
+               </Link>
           </div>
 
        </section>  
--- app-movies/src/pages/Projetos/index.jsx
+++ app-movies/src/pages/Projetos/index.jsx
@@ -1,16 +1,43 @@
+import { useState } from "react"
 import Card from "../../components/Card"
+import styles from './Projetos.module.css'
+import { useEffect } from "react"
 
 function Projetos(){
+    
+    const [repositores, setRepositories] = useState([])
+
+    useEffect(()=>{
+         const buscarRepositorio = async () =>{
+           const response = await fetch ("https://api.github.com/users/LuanRodrigues0/repos")
+           const data = await response.json() 
+           setRepositories(data)
+
+         }
+         buscarRepositorio()
+
+    },[])
+    
     return (
-        <>
+        <section className={styles.projetos}>
        
-           <h2>Projetos</h2>
-            <Card/>
-            <Card/>
-            <Card/>
-        
-        
-        </>
+         <h2>Projetos</h2>
+          {
+            repositores.length > 0 ? (
+                <section className={styles.lista}>
+                  {
+                     repositores.map((repo) =>(
+                        <Card name={repo.name}/>
+                     ))
+                  }
+                
+                 </section>
+            ) :(
+                <p>Carregando repositórios...</p>
+            )
+          }
+
+        </section>
     )
 }
 
