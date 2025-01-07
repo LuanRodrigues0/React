cfff504680d7b9975b8e0adb75c6d74bdf1c5f99
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2025-01-07 20:04:26 -0300

    responsividade

--- app-movies/src/pages/Page404/index.jsx
+++ app-movies/src/pages/Page404/index.jsx
@@ -3,7 +3,7 @@ import style from "./Page404.module.css"
 
 function Page404(){
      return(
-        <>
+        <section className={style.page404}>
              
          <h2 className={style.titulo2}>Algo de errado não está certo!</h2>
          <div className={style.textos}>
@@ -12,7 +12,7 @@ function Page404(){
       
          </div>
    
-        </>
+        </section>
      )
 
 }
