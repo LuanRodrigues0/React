b01a94296b0f5c994ef8133718c8a006f9e43ece
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2025-01-08 19:57:11 -0300

    configuração de media

--- app-movies/src/components/Header/index.jsx
+++ app-movies/src/components/Header/index.jsx
@@ -1,18 +1,37 @@
 import {Link} from 'react-router-dom'
 import styles from './Header.module.css'
+import { useState } from 'react'
 
 function Header(){
+     
+    const [showMenu, setShowMenu] = useState(false)
+    const toggleMenu = ()=>{
+        setShowMenu(!showMenu)
+    }
+
     return(
         <header className={styles.header}>
             <Link to="/">
             <span>LuanRodrigues.dev</span>
             </Link>
-            <nav>
-                <Link to="/">Home</Link>
+            <nav className={`${styles.menuSandwich} ${showMenu ? styles.show: ''}`}
+              onClick={toggleMenu}
+            >
+        
+
+               <Link to="/">Home</Link>
                <Link to="/sobre">Sobre</Link>
                <Link to="/projetos">Projetos</Link>
                <Link to="/contatos">Contatos</Link>
             </nav>
+            <div className={styles.menuButtom}
+              onClick={toggleMenu}
+            >
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+                
+            </div>
         </header>
 
     )
